2f786ba314b1d13d7ac6560aeb7fcbb88f430c5c
Denys Xavier <DenysXavier@users.noreply.github.com>
2025-03-19 19:28:24 +0000

    Getting user data from Okta

--- angular-okta-ui/src/app/profile/profile.component.ts
+++ angular-okta-ui/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserProfile } from '../shared/models/UserProfile.model';
+import { OktaAuthStateService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +8,21 @@ import { UserProfile } from '../shared/models/UserProfile.model';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   myProfile?: UserProfile;
+
+  private oktaStateService = inject(OktaAuthStateService);
+
+  ngOnInit(): void {
+    this.oktaStateService.authState$.subscribe(oktaAuthState => {
+      if (oktaAuthState.isAuthenticated) {
+        const claims = oktaAuthState.idToken?.claims;
+
+        this.myProfile = {
+          name: claims?.name,
+          email: claims?.preferred_username
+        }
+      }
+    })
+  }
 }
