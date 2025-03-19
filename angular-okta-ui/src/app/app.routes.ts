1734997e02b766d9393e7c657de865db32c4c89a
Denys Xavier <DenysXavier@users.noreply.github.com>
2025-03-19 19:32:21 +0000

    Protecting the profile page with Okta Auth Guard

--- angular-okta-ui/src/app/app.routes.ts
+++ angular-okta-ui/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { UnprotectedComponent } from './unprotected/unprotected.component';
 import { ProfileComponent } from './profile/profile.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 
 export const routes: Routes = [
     { path: '', component: UnprotectedComponent },
-    { path: 'profile', component: ProfileComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [OktaAuthGuard] },
     { path: 'login/callback', component: OktaCallbackComponent }
 ];
