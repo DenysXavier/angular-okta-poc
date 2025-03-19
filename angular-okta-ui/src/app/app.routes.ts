dea613bab8710277a16636e954650bea249b08b0
Denys Xavier <DenysXavier@users.noreply.github.com>
2025-03-19 18:53:33 +0000

    Added default callback page provided by Okta library

--- angular-okta-ui/src/app/app.routes.ts
+++ angular-okta-ui/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UnprotectedComponent } from './unprotected/unprotected.component';
 import { ProfileComponent } from './profile/profile.component';
+import { OktaCallbackComponent } from '@okta/okta-angular';
 
 export const routes: Routes = [
     { path: '', component: UnprotectedComponent },
-    { path: 'profile', component: ProfileComponent }
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login/callback', component: OktaCallbackComponent }
 ];
